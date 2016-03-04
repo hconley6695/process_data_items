e2c4101c194336837192679d158dbe8c246fe219
Heather Conley <hconley6695@gmail.com>
2016-03-04 13:29:54 -0500

    wrote code for first question

--- js/main.js
+++ js/main.js
@@ -1,7 +1,17 @@
-function averagePrice(products) {
-	var productSum = products.reduce(function(totalSum, currentPrice) {
-		return totalSum + currentPrice.price;
-	}, 0);
-	var average = productSum / products.length;
-	return average;
-}
+
+
+//introduce a variable named Sum which will add up all of the values of the properties of the objects within an array
+//introduce another variable average which will be defined by take that new value Sum and divide it by the number of values in the array
+//return the average
+
+
+var prices = items.map(function(product) {
+	return product.price;
+});
+
+var totalSum = prices.reduce(function (x, y) {
+	return x + y;
+});
+
+var average = totalSum/prices.length;
+
